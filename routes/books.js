a86ba992b182b62626a765716e4248a8b20f4b94
cecelia001 <diascece@gmail.com>
2022-12-09 15:35:05 -0500

    Delete book route

--- routes/books.js
+++ routes/books.js
@@ -6,11 +6,11 @@ const db = require("../model/helper");
 router.get('/', async function(req, res) {
   try {
     let result = await db ('SELECT * FROM books');
-    let bookList = result.data;
-    if (bookList.length === 0) {
+    let books = result.data;
+    if (books.length === 0) {
       res.status(404).send({ error: "No books" });
     } else {
-      res.send(bookList);
+      res.send(books);
     }
   } catch (err) {
     res.status(500).send({ error: err.message });
@@ -29,11 +29,30 @@ let sql = `
 try {
   await db(sql);
   let result = await db ('SELECT * FROM books');
-  let bookList = result.data;
-  res.status(201).send(bookList);
+  let books = result.data;
+  res.status(201).send(books);
 } catch (err) {
   res.status(500).send({ error: err.message });
   console.log(err.message);
+}
+});
+
+//DELETE book from list
+router.delete('/:id', async function (req, res){
+let id = req.params.id;
+
+try {
+  let result = await db(`SELECT * FROM books WHERE id=${id}`);
+  if (result.data.length === 0) {
+    res.status(404).send({ error: "Stop not found" });
+  } else {
+    await db(`DELETE FROM books WHERE id=${id}`);
+    let result = await db(`SELECT * FROM books`);
+    let books = result.data;
+    res.status(201).send(books);
+  }
+} catch (err) {
+  res.status(500).send({ error: err.message });
 }
 });
 
