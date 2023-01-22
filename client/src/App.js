5e0df7102e30d00960b3c871ec2d3b4134c1e2f1
cecelia001 <diascece@gmail.com>
2023-01-22 18:43:21 -0500

    Add jumbotron bootstrap

--- client/src/App.js
+++ client/src/App.js
@@ -91,10 +91,15 @@ async function completeBook(id) {
 
   return (
     <div className="App">
-      <div className="backgroundimg">
-        <h1> My Book List </h1>
-        <h2 className="descrip"> Keep track of what books you want to read. </h2>
-      </div>
+        <div className="jumbotron">
+          <h1 className="display-4">Hello, world!</h1>
+          <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+          <hr className="my-4"/>
+          <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+          <p className="lead">
+            <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+          </p>
+        </div>
         <AddBookForm addBookCb={addBook}/>
         <BookTable books={books} deleteBookCb={deleteBook} completeBookCb={completeBook} />
       <div className ="endtag">
