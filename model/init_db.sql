de163753e7fa78bf947414b8b4071157c69537aa
cecelia001 <diascece@gmail.com>
2023-01-24 18:02:34 -0500

    Add rating table to db

--- model/init_db.sql
+++ model/init_db.sql
@@ -5,5 +5,16 @@ CREATE TABLE `books` (
     `author` VARCHAR(100),
     `done` BOOLEAN NOT NULL
 ); 
+
 INSERT INTO `books` (title, author, done)
     VALUES ('The Silent Patient', 'Alex Michaelides', true), ('The Truth About Luck', 'Iain Reid', false), ('From the Ashes', 'Jesse Thistle', false), ('The Psychology of Time Travel', 'Kate Mascarenhas', false);
+
+
+CREATE TABLE `ratings` (
+    `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT, 
+    `book_id` INT,
+    `rating` INT
+); 
+
+INSERT INTO `ratings` (book_id, rating)
+    VALUES (1, 5);
