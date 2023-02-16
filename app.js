e6924a7fc12d8f4798a0de18eb2cae57b79d30a1
cecelia001 <diascece@gmail.com>
2023-02-16 11:52:00 -0500

    prepare app for deployment

--- app.js
+++ app.js
@@ -12,11 +12,20 @@ var app = express();
 app.use(cors());                            //added
 app.use(logger('dev'));
 app.use(express.json());
+
+//Location of static assets
+app.use(express.static(path.join(__dirname, "/client/build")));
+
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/books', booksRouter);
+
+//Respond with index.html for unmatched routes
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
 
 module.exports = app;
